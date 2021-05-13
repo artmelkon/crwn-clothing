04fd99a1860a4202da6ec21ab11bba49142951c2
Arthur Melkonyan <arthur@artmelkon.me>
2021-05-13 08:20:27 -0700

    creating reusable custom button and uppdating signin component

---
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import SignIn from '../../components/sign-in/sign-in.component';
+import './signin-signup.styles.css';
+
+
+const SingInAndSignUP = () => (
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+  </div>
+)
+
+export default SingInAndSignUP;
