27d36d969386de67459aa5a5b8cf0305536cd948
Arthur Melkonyan <arthur@artmelkon.me>
2021-05-18 20:39:47 -0700

    created user data entry to firebase

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-import './header.styles.css';
+import './header.styles.css'; // to load SVG files
 
 const Header = ({ currentUser }) => (
   <div className="header">
--- src/pages/signin-signup/signin-signup.component.jsx
+++ src/pages/signin-signup/signin-signup.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sing-up.component';
 import './signin-signup.styles.css';
 
 
 const SingInAndSignUP = () => (
-  <div className="sign-in-and-sign-up">
+  <div className="signin-signup">
     <SignIn />
+    <SignUp />
   </div>
 )
 
